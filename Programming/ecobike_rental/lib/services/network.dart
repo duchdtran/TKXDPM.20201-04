e94de0eb4ff6ad2c8e989ad74601886d96eb64cc
duchdtran <duc.td173028@sis.hust.edu.vn>
2020-11-26 00:14:49 +0700

    refs #programming: - add base api

--- Programming/ecobike_rental/lib/services/network.dart
+++ Programming/ecobike_rental/lib/services/network.dart
@@ -1,3 +1,27 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../data/response/process_transaction.dart';
 class ApiHelper{
+  static const  String baseURL = 'http://ecopark-system-api.herokuapp.com';
+  Future<ProcessTransactionResponse> processTransaction() async {
+    final response = await http.patch('$baseURL/api/card/processTransaction');
+
+    if (response.statusCode == 200) {
+      //return ProcessTransactionResponse.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load album');
+    }
+  }
+  Future<ProcessTransactionResponse> resetBalance() async {
+    final response = await http.patch('$baseURL/api/card/processTransaction');
+
+    if (response.statusCode == 200) {
+      //return ProcessTransactionResponse.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load album');
+    }
+  }
 
 }
